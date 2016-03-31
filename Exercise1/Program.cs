e0c0fc88b3cad6eb5b876ba0fb6ed440e3e8ab4f
ddovdon <ddovdon@gmail.com>
2016-04-01 05:58:18 +0800

    commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        const int iTwo = 2, iSeven = 7;
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=100; i++)
+            {
+                if (IsMultiples(iTwo, i) && IsMultiples(iSeven, i))
+                {
+                    Console.WriteLine("CrossShield");
+                }
+                else if (IsMultiples(iTwo, i))
+                {
+                    Console.WriteLine("Cross");
+                }
+                else if (IsMultiples(iSeven, i))
+                {
+                    Console.WriteLine("Shield");
+                }
+                else
+                {
+                    Console.WriteLine("Number: {0}", i);
+                }
+            }
+            Console.ReadKey();
+        }        
+
+        static bool IsMultiples(int number, int index)
+        {
+            return (index % number) == 0;
+        }
+    }
+}
